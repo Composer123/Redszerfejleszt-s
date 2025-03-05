b1f65e69902f29fcd86c0abec328696624fa9018
Lori <Lori@DESKTOP-532UIPB>
2025-03-05 16:08:50 +0100

    asdasd

--- Raktar/Address.cs
+++ Raktar/Address.cs
@@ -14,4 +14,6 @@ public partial class Address
     public virtual SimpleAddress? SimpleAddress { get; set; }
 
     //This is a comment made by Gergo who is indeed a gamer (If you want some drawings, my commisions are open)
+
+    //kumbej
 }
