63cafcea8e53991e875043843e319d930a91282c
Gergő Kardos <kardosgergo3@gmail.com>
2025-03-05 15:49:46 +0100

    I added a comment to Address.

--- Raktar/Address.cs
+++ Raktar/Address.cs
@@ -12,4 +12,6 @@ public partial class Address
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 
     public virtual SimpleAddress? SimpleAddress { get; set; }
+
+    //This is a comment made by Gergo who is indeed a gamer (If you want some drawings, my commisions are open)
 }
