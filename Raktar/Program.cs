f6ebf017e76df37cfb285b77a81bcae61ab11a4c
Takacs Gergely <takacs.gergely@liveedu.mathiasz.hu>
2025-03-26 14:59:13 +0100

    Add auto swagger main app.

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -13,11 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<WarehouseDbContext>(options =>
 {
     options.UseSqlServer("Server=localhost;Database=WarehouseDB;Trusted_Connection=True;TrustServerCertificate=True;");
 });
-
 builder.Services.AddScoped<IAddressService, AddressService>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
@@ -29,6 +30,13 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseHttpsRedirection();
 
